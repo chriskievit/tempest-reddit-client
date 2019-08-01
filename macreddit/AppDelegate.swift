d531d47abc9406df16d707422093ccd14ee0b094
Chris Kievit <ckievit@pro4all.nl>
2019-08-01 15:43:38 +0200

    Initial Commit

---
+++ macreddit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macreddit
+//
+//  Created by Chris on 01/08/2019.
+//  Copyright © 2019 Tempest. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macreddit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macreddit
+//
+//  Created by Chris on 01/08/2019.
+//  Copyright © 2019 Tempest. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
