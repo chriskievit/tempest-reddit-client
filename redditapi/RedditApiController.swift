620eea76839a1db25af9154a4ae45abbd06a17cb
Chris Kievit <ckievit@pro4all.nl>
2019-08-02 12:09:20 +0200

    Basic setup for "abstract" ApiController

--- redditapi/RedditApiController.swift
+++ redditapi/RedditApiController.swift
@@ -8,3 +8,49 @@
 
 import Foundation
 
+enum RequestResult: Error {
+    case ServerError
+    case UserError
+}
+
+protocol ApiController {
+}
+
+extension ApiController {
+    func performGetRequest(url: String, result: (Result<Array<String>, RequestResult>) -> Void) {
+        if !url.isEmpty {
+            result(.success(["Result was successful"]))
+        } else {
+            result(.failure(RequestResult.ServerError))
+        }
+    }
+    
+    func decode<T: Decodable>(jsonString: String) -> T? {
+        guard !jsonString.isEmpty else {
+            return nil
+        }
+        
+        let jsonData: Data? = jsonString.data(using: .utf8)
+        
+        guard let data = jsonData else {
+            return nil
+        }
+        
+        return try? JSONDecoder().decode(T.self, from: data)
+    }
+}
+
+class RedditApiController: ApiController {
+    func getTheThing() {
+        performGetRequest(url: "loginUrl") { (result) in
+            switch result {
+            case .success(let response):
+                print(response)
+                break
+            case .failure(_):
+                print("There was an error")
+                break
+            }
+        }
+    }
+}
